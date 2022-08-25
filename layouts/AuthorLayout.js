902967bbac4d0d389f38df065e6a026522cd63a6
uniplac <82620049+uniplac@users.noreply.github.com>
2022-08-25 17:31:08 -0300

    profile

--- layouts/AuthorLayout.js
+++ layouts/AuthorLayout.js
@@ -3,33 +3,34 @@ import Image from "next/image"
 import projectsData from '@/data/projectsData'
 import Card from '@/components/Card'
 
+
 export default function AuthorLayout({ children, frontMatter }) {
   const { name, occupation, company } = frontMatter
-
   return (
     <>
       <PageSEO title={`About - ${name}`} description={`About me - ${name}`} />
       <div className="divide-y">
         <div className="pt-6 pb-8 space-y-2 md:space-y-5">
-          <h1 className=" text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Sobre Kevelin
+          <h1 className=" text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14 flex">
+            <Image src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="42" height="42"></Image> Sobre Thiago
           </h1>
         </div>
         <div className="items-start space-y-2 xl:grid xl:grid-cols-3 xl:gap-x-8 xl:space-y-0">
           <div className="flex flex-col items-center pt-8 space-x-2">
             <img
-              src="https://github.com/KevelinMM.png"
+              src="https://github.com/thiagosartorel.png"
               alt="avatar"
               className="w-48 h-48 rounded-full"
             />
-            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight">{name}</h3>
+            <h3 className="pt-4 pb-2 text-2xl font-bold leading-8 tracking-tight text-center">
+              {name}
+            </h3>
             <div className="text-gray-500 dark:text-gray-400">{occupation}</div>
             <div className="text-gray-500 dark:text-gray-400">{company}</div>
           </div>
           <div className="pt-8 pb-8 prose dark:prose-dark max-w-none xl:col-span-2 text-justify">
             {children}
           </div>
-
         </div>
         <div className="">
           <h1 className="text-center text-2xl font-bold py-4">Últimos trabalhos</h1>
