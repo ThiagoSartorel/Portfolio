b0bd5df8f34e0f66409510b2abb7f555275a3bf9
ThiagoSartorel <thiagosartorel@uniplac.net>
2022-08-25 19:20:18 -0300

    style card

--- components/Card.js
+++ components/Card.js
@@ -14,7 +14,7 @@ const Card = ({ title, description, imgSrc, href }) => {
 
   return (
     <div className="p-4 md:w-1/2 md" style={{ maxWidth: '544px' }}>
-      <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700">
+      <div className="h-full overflow-hidden border-2 border-gray-200 rounded-md border-opacity-60 dark:border-gray-700 bg-white bg-opacity-10 hover:bg-opacity-70 hover:text-black dark:hover:shadow-primary-800 shadow-md hover:scale-105 group cursor-pointer">
         {href ? (
           <Link href={href} aria-label={`Link to ${title}`}>
             <Image
